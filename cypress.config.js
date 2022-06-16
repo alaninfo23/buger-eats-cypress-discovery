826ddd20d7ff38f851d1ae2e97c3b089b5dd6697
alaninfo23 <js-alan@hotmail.com>
2022-06-16 11:46:38 -0300

    adicionando informações do dashboards do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+    projectId: "dgs3i9",
   //padronizar a resolução do cypress nesse projeto
     "viewportWidth": 1440,
     "viewportHeight": 900,
